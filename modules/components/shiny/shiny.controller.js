5f12044d5e010b291768d525f99690225ab2cf7f
antogyn <antogyn@gmail.com>
2016-01-27 20:36:40 +0100

    Simplify shiny controller

--- modules/components/shiny/shiny.controller.js
+++ modules/components/shiny/shiny.controller.js
@@ -1,12 +1,7 @@
 export default class ShinyController {
   constructor(ShinyService) {
-    this._ShinyService = ShinyService;
-    this.init();
-  }
-
-  init() {
-    this.addResult = this._ShinyService.add(2, 2);
-    this.substractResult = this._ShinyService.substract(15 , 4);
+    this.addResult = ShinyService.add(2, 2);
+    this.substractResult = ShinyService.substract(15, 4);
   }
 }
 
